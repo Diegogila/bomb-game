1c613b9e13a92e07963f8cc1237b8b1e08d005ea
diegogil <dgg26496@hotmail.com>
2022-11-03 13:55:14 -0600

    Se le agrega configuracion al display del juego

--- src/game.js
+++ src/game.js
@@ -4,6 +4,11 @@ const game = canvas.getContext('2d');
 
 window.addEventListener('load',startGame);
 function startGame(){
- game.fillRect(0,0,100,100);
- game.clearRect(50,50,50,50)
+    canvas.setAttribute('width', window.innerWidth * 0.75);
+    canvas.setAttribute('height', window.innerHeight * 0.5);
+    
+    //window.innerHeight
+    window.innerWidth
+ /*game.fillRect(0,0,100,100);
+ game.clearRect(50,50,50,50);*/
 }
