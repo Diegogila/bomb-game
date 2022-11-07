19465d83099942f739360eb227080c5afe4a66dc
diegogil <dgg26496@hotmail.com>
2022-11-07 14:20:06 -0600

    se modifica logica

--- src/game.js
+++ src/game.js
@@ -8,13 +8,25 @@ function startGame(){
     if (window.innerHeight > window.innerWidth) {
         canvasSize = window.innerWidth * 0.75;
     } else {
-        canvasSize = window.innerHeight *0.5;
+        canvasSize = window.innerHeight * 0.5;
     }
     canvas.setAttribute('width', canvasSize);
     canvas.setAttribute('height', canvasSize);
+
+    const elementSize = canvasSize/10;
+    console.log({canvasSize, elementSize}); 
+    game.font = `${elementSize}px Verdana`;
+    game.textAlign = `end`;
+    game.fillText(emojis['X'],elementSize,elementSize);
+    
+
+    /*for (let i = 1; i <= 10; i++) {
+        game.fillText(emojis['X'],elementSize * i,elementSize);
+        console.log(i);
+    }*/
     
     //window.innerHeight
-    window.innerWidth
+    //window.innerWidth
  /*game.fillRect(0,0,100,100);
  game.clearRect(50,50,50,50);*/
 }
