9732190bcfbfa8b5c3c0b5f090c27597aa3830e3
diegogil <dgg26496@hotmail.com>
2022-12-17 13:28:10 -0600

    Se termina logica de inicio y final de juego

--- src/game.js
+++ src/game.js
@@ -1,3 +1,50 @@
+class Player{
+    constructor(){
+        this.x = undefined;
+        this.y = undefined;
+        this.initialPotition = {};
+        this.colicionObj = undefined;
+    }
+    move(map,y,x){
+        console.log(this.x);
+        console.log(this.y);
+        this.colicionObj = map[this.y+y][this.x+x];
+        if (this.y+y <0 || this.x+x <0) {
+            map[this.y][this.x] = "I";
+        } else if(this.colicionObj == "-"){
+            map[this.y+y][this.x+x] = "I";
+            map[this.y][this.x] = this.colicionObj;   
+        }
+        console.log(this.x);
+        console.log(this.y);
+    }
+}
+
+class Map{
+    constructor(player,elSize){
+        this.level = 0;
+        this.maps = maps;
+        this.mapCols;
+        this.player = player;
+        this.elSize = elSize;
+    }
+    mapBuild(){
+        const map = maps[this.level];
+        const mapRows = map.trim().split('\n');
+        this.mapCols = mapRows.map(row => row.trim().split(''));
+    }
+    render(game){
+        this.mapCols.forEach((row, y) => {
+            row.forEach((col, x) => {
+                if (col == "I") {
+                    this.player.y = y;
+                    this.player.x = x;
+                }
+                game.fillText(emojis[col],this.elSize * (x+1), this.elSize * (y+1));})
+            }) 
+    }
+}
+
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
@@ -5,31 +52,9 @@ const game = canvas.getContext('2d');
 window.addEventListener('load',startGame);
 
 
-class Player{
-    constructor(icon, x, y){
-        this.icon = icon;
-        this.x = undefined;
-        this.y = undefined;
-        this.initialPotition = {};
-    }
-    move(map,y,x,render){
-        let colObj = map[this.y+y][this.x+x];
-        if (colObj == "X") {
-            map[this.y][this.x] = "-";
-            map[this.initialPotition.y][this.initialPotition.x]='I';
-        } else if(this.y+y < 0 || this.x+x < 0){
-            map[this.y][this.x] = "I";
-        }else{
-            map[this.y+y][this.x+x] = "I";
-            map[this.y][this.x] = colObj;   
-        }
-        render(map); 
-    }
-}
 
 
-
-function startGame(){
+function startGame(level){
     let canvasSize;
     if (window.innerHeight > window.innerWidth) {
         canvasSize = window.innerWidth * 0.75;
@@ -40,70 +65,56 @@ function startGame(){
     canvas.setAttribute('height', canvasSize);
 
     const elementSize = canvasSize/10;
-    console.log({canvasSize, elementSize}); 
     game.font = `${elementSize}px Verdana`;
     game.textAlign = `end`;
 
-    const map = maps[1];
-    const mapRows = map.trim().split('\n');
-    const mapCols = mapRows.map(row => row.trim().split(''));
+    const player = new Player();
+    const map = new Map(player,elementSize);
+    map.mapBuild();
+    map.render(game);
 
-    const player = new Player(emojis["I"]);
-
-    function render(map){
-        map.forEach((row, y) => {
-            row.forEach((col, x) => {
-                if (col == "I") {
-                    player.y = y;
-                    player.x = x;
-                    
-                }
-                game.fillText(emojis[col],elementSize * (x+1), elementSize * (y+1));})
-            }) 
-    }
-    render(mapCols);
     player.initialPotition = {
         y:player.y,
         x:player.x};
-        console.log(player);
-    
-        
+         
     window.addEventListener('keydown', (event) => {
-        console.log(event.key)
         game.clearRect(0,0,canvasSize,canvasSize);
         switch (event.key) {
             case "ArrowUp":
-                player.move(mapCols,-1,0,render);
+                player.move(map.mapCols,-1,0);
                 break;
             case "ArrowRight":
-                player.move(mapCols,0,1,render);
+                player.move(map.mapCols,0,1);
                 break;
             case "ArrowDown":
-                player.move(mapCols,1,0,render);
+                player.move(map.mapCols,1,0);
                 break;
             case "ArrowLeft":
-                player.move(mapCols,0,-1,render);
-                /*colicionObj = mapCols[player.y-1][player.x-2];
-                if (colicionObj != "X") {
-                    mapCols[player.y-1][player.x-2] = "I";
-                    mapCols[player.y-1][player.x-1] = colicionObj;   
-                } else{
-                    mapCols[player.y-1][player.x-1] = "-";
-                    mapCols[player.initialPotition.y-1][player.initialPotition.x-1]='I';
-                }*/
+                player.move(map.mapCols,0,-1);
                 break;                
-                
                 default:
                     break;
-                }
-    
-        render(mapCols); 
-        console.log(player);
-    });   
-    /*for (let row = 1; row <= 10; row++) {
-        for (let col = 1; col <= 10; col++) {
-            game.fillText(emojis[mapCols[row - 1][col - 1]],elementSize * col,elementSize * row);   
         }
-    }*/
+        if (player.colicionObj == "X") {
+            alert('Perdiste, hay que ser muy idiota para perder en esto');
+            map.mapCols[player.y][player.x] = "-";
+            map.mapCols[player.initialPotition.y][player.initialPotition.x]='I';
+            player.colicionObj = undefined;           
+        }else if(player.colicionObj == "O"){
+            if (map.level<2) {
+                map.level++;
+            }else{
+                alert('Ganaste, recoge tu premio y vete al carajo');
+                map.level = 0;
+            }
+            map.mapBuild();
+            map.render(game);
+            player.colicionObj = undefined;
+            player.initialPotition = {
+                y:player.y,
+                x:player.x};
+        }
+        map.render(game); 
+    });   
 }
 
